c5f37ce328a8688afa6304ba8c6a59f625653684
Elena Vorontsova <lena.voroncova26@yandex.ru>
2022-07-26 15:21:54 +0400

    add: add the email textField

--- Messenger/Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Messenger/Controllers/Login/LoginViewController.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    private let scrollView: UIScrollView = { () -> UIScrollView in
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let emailTF: UITextField = { () -> UITextField in
+        let textField = UITextField()
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.returnKeyType = .continue
+        textField.layer.cornerRadius = 12
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.placeholder = "Email Address..."
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 0))
+        textField.leftViewMode = .always
+        textField.backgroundColor = .white
+        return textField
+    }()
+    
     private let imageView: UIImageView = { () -> UIImageView in
         let imageView = UIImageView()
         imageView.image = UIImage(named: "logo")
@@ -29,17 +50,26 @@ class LoginViewController: UIViewController {
             action: #selector(didTapRegister))
         
         // Add subviews
-        view.addSubview(imageView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailTF)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.width/3
+        scrollView.frame = view.bounds
+        let size = scrollView.width/3
+        
         imageView.frame = CGRect(
-            x: (view.width-size)/2,
-            y: 90,
+            x: (scrollView.width-size)/2,
+            y: 20 ,
             width: size,
             height: size)
+        emailTF.frame = CGRect(
+            x: 30,
+            y: imageView.bottom + 10,
+            width: scrollView.width - 60,
+            height: 52)
     }
     
     @objc private func didTapRegister() {
