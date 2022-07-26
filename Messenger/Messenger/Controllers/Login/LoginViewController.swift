26dfa330635fde58abd3632bf2ed1b2c19ca5e9a
Elena Vorontsova <lena.voroncova26@yandex.ru>
2022-07-26 14:43:01 +0400

    add: add the imageView in login's page

--- Messenger/Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Messenger/Controllers/Login/LoginViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let imageView: UIImageView = { () -> UIImageView in
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,19 @@ class LoginViewController: UIViewController {
             style: .done,
             target: self,
             action: #selector(didTapRegister))
+        
+        // Add subviews
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.width/3
+        imageView.frame = CGRect(
+            x: (view.width-size)/2,
+            y: 90,
+            width: size,
+            height: size)
     }
     
     @objc private func didTapRegister() {
