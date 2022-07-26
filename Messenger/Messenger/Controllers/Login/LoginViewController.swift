9dfa104217750e6f40587221ceb76feb635c1d6c
Elena Vorontsova <lena.voroncova26@yandex.ru>
2022-07-26 14:24:29 +0400

    add: create the navigation between login's and register's pages

---
+++ Messenger/Messenger/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  Messenger
+//
+//  Created by Admin on 25.07.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Log In"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Register",
+            style: .done,
+            target: self,
+            action: #selector(didTapRegister))
+    }
+    
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+}
---
+++ Messenger/Messenger/Controllers/Login/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Admin on 25.07.2022.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+    
+
+}
